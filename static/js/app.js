254f27aa289e982fdfa5f05001c1b3e6ad7ca4fa
briannaso <sosa.brianna8@gmail.com>
2021-10-25 22:43:59 -0400

    created buildTable function

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,8 @@ const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+// create a function called buildTable, pass in data
+function buildTable(data) {
+    tbody.html("");
+}
